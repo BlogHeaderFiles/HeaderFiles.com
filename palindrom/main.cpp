73f08f2b3f8d7408211ecda5f191a733429cb298
Carlos Buchart <cbuchart@users.noreply.github.com>
2020-02-18 08:43:17 +0100

    Fix typo in function name

--- palindrom/main.cpp
+++ palindrom/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <cstring>
 
-bool is_palindrom(const char* str, size_t len)
+bool is_palindrome(const char* str, size_t len)
 {
     if (len == 0) return true;
     for (size_t ii = 0, jj = len - 1; ii < jj; ++ii, --jj) {
@@ -12,24 +12,24 @@ bool is_palindrom(const char* str, size_t len)
 }
 
 template<typename T>
-bool is_palindrom(const T& str)
+bool is_palindrome(const T& str)
 {
     return std::equal(begin(str), end(str), rbegin(str));
 }
 
 template<size_t N>
-bool is_palindrom(const char (&str)[N])
+bool is_palindrome(const char (&str)[N])
 {
-    return is_palindrom(str, N - 1);
+    return is_palindrome(str, N - 1);
 }
 
 template<size_t N>
 void test(const char (&txt)[N])
 {
     const std::string str(txt);
-    std::cout << str << " is palindrom (const char*): " << is_palindrom(str.c_str(), str.size()) << "\n";
-    std::cout << str << " is palindrom (std::string): " << is_palindrom(str) << "\n";
-    std::cout << str << " is palindrom (const char[]): " << is_palindrom(txt) << "\n";
+    std::cout << str << " is palindrome (const char*): " << is_palindrome(str.c_str(), str.size()) << "\n";
+    std::cout << str << " is palindrome (std::string): " << is_palindrome(str) << "\n";
+    std::cout << str << " is palindrome (const char[]): " << is_palindrome(txt) << "\n";
 }
 
 int main()
@@ -42,6 +42,4 @@ int main()
     test("aaaa");
     test("Ojo");
     test("HeaderFiles");
-    
-    return 0;
 }
